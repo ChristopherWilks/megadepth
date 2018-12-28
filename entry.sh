f8f6e3151586e4a550714dba435b391c09653ca4
Ben Langmead <langmea@cs.jhu.edu>
2018-12-28 15:47:31 -0500

    fix script to work with subdirs

--- entry.sh
+++ entry.sh
@@ -13,8 +13,9 @@ pushd ${DR}
 cmake -DCMAKE_BUILD_TYPE=Release ..
 make
 ldd -v bamcount
-./bamcount
+./bamcount --help
 popd
+cp ${DR}/bamcount ./bamcount
 rm -rf ${DR}
 
 DR=build-debug-temp
@@ -23,11 +24,10 @@ pushd ${DR}
 cmake -DCMAKE_BUILD_TYPE=Debug ..
 make
 ldd -v bamcount
-./bamcount
+./bamcount --help
 popd
+cp ${DR}/bamcount ./bamcount-debug
 rm -rf ${DR}
 
-cp build-release/bamcount ./bamcount
-cp build-debug/bamcount ./bamcount-debug
 zip bamcount.zip bamcount bamcount-debug
 rm -f bamcount bamcount-debug
