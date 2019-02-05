ccae0e31a3aa82f8b1ca0a1acd6bc07f993a17f2
Chris Wilks <broadsword@gmail.com>
2019-02-05 18:28:58 -0500

    remove curl as a dependency for libBigWig

--- get_libBigWig.sh
+++ get_libBigWig.sh
@@ -10,6 +10,8 @@ curl -L https://github.com/dpryan79/libBigWig/archive/${AR} > $FN
 tar -zxvf $FN
 rm -f ${FN}
 pushd $DIR
+cp Makefile Makefile.bak
+cat Makefile.bak | perl -ne 'chomp; $f=$_; $f=~s/^(CFLAGS.+)$/$1 -DNOCURL/; $f=~s/^(LIBS =.*)(-lcurl)/$1/; print "$f\n";' > Makefile
 make
 popd
 mv $DIR libBigWig
