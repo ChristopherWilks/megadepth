68d008a802a357f9fa908bf53913ada025e59581
ChristopherWilks <broadsword@gmail.com>
2019-03-16 23:36:13 -0400

    updated tests/test.sh to call bcd_runner

--- tests/test.sh
+++ tests/test.sh
@@ -2,7 +2,7 @@
 
 rm -f test.bam.auc.tsv test.bam.all.tsv test.bam.unique.tsv test.bam.frags.tsv test.bam.alts.tsv test.bam.softclip.tsv
 
-time ./bcd tests/test.bam --threads 4 --coverage --no-head --bigwig test.bam --auc test.bam --min-unique-qual 10 --annotation tests/test_exons.bed test.bam --frag-dist test.bam --alts test.bam --include-softclip test.bam --only-polya --read-ends test --test-polya > test_run_out 2>&1
+time ./bcd_runner tests/test.bam --threads 4 --coverage --no-head --bigwig test.bam --auc test.bam --min-unique-qual 10 --annotation tests/test_exons.bed test.bam --frag-dist test.bam --alts test.bam --include-softclip test.bam --only-polya --read-ends test --test-polya > test_run_out 2>&1
 
 diff <(sort tests/test.bam.orig.frags.tsv) <(sort test.bam.frags.tsv)
 diff tests/test.bam.orig.alts.tsv test.bam.alts.tsv
