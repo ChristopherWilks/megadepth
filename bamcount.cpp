a55e4ba93d0db56d1a3a2a401a28dca9390f21e8
ChristopherWilks <broadsword@gmail.com>
2020-04-02 02:07:19 -0400

    first attempt at splitting by chromosome-per-thread

--- bamcount.cpp
+++ bamcount.cpp
@@ -64,7 +64,7 @@ static const char** get_option(
     return itr + shift + 1;
 }
 
-void process_bam_worker(const int worker_idx, const char* bam_arg, bam_hdr_t *hdr_) {
+void process_bam_worker(const int worker_idx, const char* bam_arg, char* region) {
     htsFile *bam_fh = sam_open(bam_arg, "r");
     if(!bam_fh) {
         std::cerr << "ERROR: Could not open " << bam_arg << ": "
@@ -78,18 +78,36 @@ void process_bam_worker(const int worker_idx, const char* bam_arg, bam_hdr_t *hd
                   << std::strerror(errno) << std::endl;
         return;
     }
-    bam_hdr_t *hdr = sam_hdr_read(bam_fh);
-    uint64_t recs = 0;
-    while(sam_read1(bam_fh, hdr, rec) >= 0) {
-        recs++;
-        bam1_core_t *c = &rec->core;
-        //read name
-        char* qname = bam_get_qname(rec);
+    bam_hdr_t* hdr = sam_hdr_read(bam_fh);
+    hts_idx_t* idx = nullptr;
+    //char* fn_idx_in = nullptr;
+    //idx = sam_index_load2(bam_fh, fam_arg, fn_idx_in); // load index
+    idx = sam_index_load(bam_fh, bam_arg);
+    if (idx == 0) { // index is unavailable
+        fprintf(stderr, "[main_samview] random alignment retrieval only works for indexed BAM or CRAM files.\n");
+        return;
     }
+    //hts_idx_t *sam_index_load(htsFile *fp, const char *fn)
+    //hts_itr_t *sam_itr_queryi(const hts_idx_t *idx, int tid, int beg, int end)
+    hts_itr_t *iter = sam_itr_querys(idx, hdr, region); // parse a region in the format like `chr2:100-200'
+    uint64_t recs = 0;
+ // fetch alignments
+    int result;
+    while ((result = sam_itr_next(bam_fh, iter, rec)) >= 0) {
+    //while(sam_read1(bam_fh, hdr, rec) >= 0) {
+        recs++;
+        //bam1_core_t *c = &rec->core;
+        //read name
+        //char* qname = bam_get_qname(rec);
+    }
+    hts_itr_destroy(iter);
+    bam_destroy1(rec);
+    hts_idx_destroy(idx); // destroy the BAM index
     fprintf(stdout,"Worker %d read %lu records\n",worker_idx,recs);
 }
 
 int go(const char* bam_arg, int argc, const char** argv, htsFile *bam_fh, bool is_bam) {
+
     int nthreads = 4;
     if(has_option(argv, argv+argc, "--threads")) {
         const char** nthreads_ = get_option(argv, argv+argc, "--threads");
@@ -101,9 +119,18 @@ int go(const char* bam_arg, int argc, const char** argv, htsFile *bam_fh, bool i
                   << ": " << std::strerror(errno) << std::endl;
         return 1;
     }
+    char** regions = new char*[nthreads];
+    regions[0] = new char[50];
+    regions[0] = "chr1:1-260000000";
+    regions[1] = new char[50];
+    regions[1] = "chr2:1-260000000";
+    regions[2] = new char[50];
+    regions[2] = "chr3:1-260000000";
+    regions[3] = new char[50];
+    regions[3] = "chr4:1-260000000";
     std::vector<std::thread> threads;
     for(int i=0; i < nthreads; i++) 
-            threads.push_back(std::thread(process_bam_worker, i, std::ref(bam_arg), std::ref(hdr)));
+            threads.push_back(std::thread(process_bam_worker, i, std::ref(bam_arg), std::ref(regions[i])));
     for(int i=0; i < threads.size(); i++)
         threads[i].join();
     uint64_t recs = 0;
