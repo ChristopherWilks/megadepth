4b6e8e15521bebeadf8aecc07041345e9604984b
ChristopherWilks <broadsword@gmail.com>
2020-06-12 11:39:58 -0400

    added statlib compile option for dynamic bin w/ htslib & libBigWig statically linked

--- build_no_container.sh
+++ build_no_container.sh
@@ -4,7 +4,7 @@ set -e
 
 #build dynamic by default
 build_type=$1
-bc=`perl -e '$bt="'$build_type'"; if($bt=~/static/i) { print "megadepth_static"; } else { print "megadepth_dynamic"; }'`
+bc=`perl -e '$bt="'$build_type'"; if($bt=~/static/i) { print "megadepth_static"; } elsif($bt=~/statlib/i) { print "megadepth_statlib"; } else { print "megadepth_dynamic"; }'`
 
 if [[ ! -s zlib ]] ; then
     ./get_zlib.sh
@@ -19,6 +19,32 @@ if [[ ! -s libBigWig ]] ; then
 fi
 
 set -x
+
+#compile a no-curl static version of libBigWig
+if [[ $bc == 'megadepth_static' ]]; then
+    pushd libBigWig
+    make clean
+    make -f Makefile.nocurl lib-static
+    popd
+fi
+
+#compile a position indenpendent code static version of libBigWig
+#but *with* curl calls present, to be resolved later in the
+#dynamic linking of megadepth
+if [[ $bc == 'megadepth_statlib' ]]; then
+    pushd libBigWig
+    make clean
+    make -f Makefile.fpic lib-static
+    popd
+fi
+
+#compile a the original, dynamic version of libBigWig
+if [[ $bc == 'megadepth_dynamic' ]]; then
+    pushd libBigWig
+    make clean
+    make -f Makefile.orig lib-shared
+    popd
+fi
 
 export LD_LIBRARY_PATH=./htslib:./libBigWig:$LD_LIBRARY_PATH
 
