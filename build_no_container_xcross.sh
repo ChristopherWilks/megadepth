67583f32162de3b4bf1ef552827e214f32f0eec1
ChristopherWilks <broadsword@gmail.com>
2020-07-01 16:31:41 -0400

    adding osxcross support for x-compiling for MacOS

---
+++ build_no_container_xcross.sh
@@ -0,0 +1,80 @@
+#!/usr/bin/env bash
+
+set -e
+
+export APPLE_VER=12
+OSXCROSS_ROOT=/opt/osxcross/target/bin
+export PATH=$OSXCROSS_ROOT:$PATH
+export OSX_ARCH=x86_64
+export OSX_CC=o64-gcc
+export OSX_CXX=o64-g++
+export CC=${OSX_CC}
+export CXX=${OSX_CXX}
+export AR=${OSXCROSS_ROOT}/$OSX_ARCH-apple-darwin${APPLE_VER}-ar
+export RANLIB=${OSXCROSS_ROOT}/$OSX_ARCH-apple-darwin${APPLE_VER}-ranlib
+
+#build dynamic by default
+build_type=$1
+bc=`perl -e '$bt="'$build_type'"; if($bt=~/static/i) { print "megadepth_static"; } elsif($bt=~/statlib/i) { print "megadepth_statlib"; } else { print "megadepth_dynamic"; }'`
+
+if [[ ! -e htslib/libhts.a ]] ; then
+    ./get_htslib.sh $OSX_ARCH-apple-darwin${APPLE_VER}-gcc
+fi
+
+if [[ ! -s libBigWig ]] ; then
+    ./get_libBigWig.sh
+fi
+
+set -x
+
+#compile a no-curl static version of libBigWig
+if [[ $bc == 'megadepth_static' ]]; then
+    pushd libBigWig
+    make clean
+    make -f Makefile.nocurl lib-static
+    popd
+fi
+
+#compile a position indenpendent code static version of libBigWig
+#but *with* curl calls present, to be resolved later in the
+#dynamic linking of megadepth
+if [[ $bc == 'megadepth_statlib' ]]; then
+    pushd libBigWig
+    make clean
+    make -f Makefile.fpic lib-static
+    popd
+fi
+
+#compile a the original, dynamic version of libBigWig
+if [[ $bc == 'megadepth_dynamic' ]]; then
+    pushd libBigWig
+    make clean
+    make -f Makefile.orig lib-shared
+    popd
+fi
+
+export LD_LIBRARY_PATH=./htslib:./libBigWig:$LD_LIBRARY_PATH
+
+DR=build-release-temp
+mkdir -p ${DR}
+pushd ${DR}
+#CC=${OSX_CC} CXX=${OSX_CXX} cmake -DCMAKE_BUILD_TYPE=Release -DCMAKE_AR=/opt/osxcross/target/bin/$OSX_ARCH-apple-darwin${APPLE_VER}-ar -DCMAKE_RANLIB=/opt/osxcross/target/bin/$OSX_ARCH-apple-darwin${APPLE_VER}-ranlib -D CMAKE_C_COMPILER=${OSX_CC} -D CMAKE_CXX_COMPILER=${OSX_CXX} ..
+#CC=${OSX_CC} CXX=${OSX_CXX} AR=/opt/osxcross/target/bin/$OSX_ARCH-apple-darwin${APPLE_VER}-ar RANLIB=/opt/osxcross/target/bin/$OSX_ARCH-apple-darwin${APPLE_VER}-ranlib make ${bc}
+cmake -DCMAKE_BUILD_TYPE=Release -DCMAKE_AR=${AR} -DCMAKE_RANLIB=${RANLIB} -D CMAKE_C_COMPILER=${OSX_CC} -D CMAKE_CXX_COMPILER=${OSX_CXX} ..
+make ${bc}
+popd
+cp ${DR}/${bc} ./
+ln -fs ./$bc megadepth
+./megadepth --version
+rm -rf ${DR}
+
+DR=build-debug-temp
+mkdir -p ${DR}
+pushd ${DR}
+cmake -DCMAKE_BUILD_TYPE=Debug -DCMAKE_AR=${AR} -DCMAKE_RANLIB=${RANLIB} -D CMAKE_C_COMPILER=${OSX_CC} -D CMAKE_CXX_COMPILER=${OSX_CXX} ..
+make ${bc}
+popd
+cp ${DR}/${bc} ./${bc}_debug
+ln -fs ./${bc}_debug megadepth_debug
+./megadepth_debug --version
+rm -rf ${DR}
--- get_htslib.sh
+++ get_htslib.sh
@@ -2,15 +2,40 @@
 
 set -ex
 
+#e.g. i386-apple-darwin1 (CC=o32-gcc)
+#or x86_64-apple-darwin12-gcc (CC=o64-gcc)
+#CC must be set as well
+xcross=$1
+
 VER=1.9
 AR=htslib-${VER}.tar.bz2
-curl -OL https://github.com/samtools/htslib/releases/download/${VER}/${AR}
-bzip2 -dc ${AR} | tar xvf - 
-rm -f ${AR}
-pushd htslib-${VER}
+if [[ ! -s htslib ]] ; then
+    curl -OL https://github.com/samtools/htslib/releases/download/${VER}/${AR}
+    bzip2 -dc ${AR} | tar xvf - 
+    rm -f ${AR}
+    pushd htslib-${VER}
+    MOVE=1
+else
+    pushd htslib
+fi
 autoheader
 autoconf
-./configure --disable-bz2 --disable-lzma --disable-libcurl --without-libdeflate
-make
+make clean
+if [[ -z $xcross ]]; then
+    ./configure --disable-bz2 --disable-lzma --disable-libcurl --without-libdeflate
+    make
+else
+    ./configure --disable-bz2 --disable-lzma --disable-libcurl --without-libdeflate --host=$xcross
+    #only make static lib for cross-compilation for now
+    export AR=/opt/osxcross/target/bin/x86_64-apple-darwin12-ar
+    export RANLIB=/opt/osxcross/target/bin/x86_64-apple-darwin12-ranlib
+    cp Makefile Makefile.orig
+    cat Makefile.orig | perl -ne 'chomp; $f=$_; $f=~s!^(AR\s*=\s*ar\s*)$!AR='$AR'!; $f=~s!^(RANLIB\s*=\s*ar\s*)$!AR='$RANLIB'!; print "$f\n";' > Makefile
+    make libhts.a
+fi
+
 popd
-mv htslib-${VER} htslib
+
+if [[ -n "$MOVE" ]]; then
+    mv htslib-${VER} htslib
+fi
