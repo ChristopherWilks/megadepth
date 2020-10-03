67314566e29c11bed46869d209ad52ef337c34a8
ChristopherWilks <broadsword@gmail.com>
2020-10-03 16:17:12 -0400

    update to run streamlined builds for all 3 platforms

--- release.sh
+++ release.sh
@@ -1,5 +1,7 @@
 #!/usr/bin/env bash
 #build "statlib" version (or main linux version) of megadepth
 ./run_hbb_build_container.sh
+./run_macos_build_container.sh
+./run_windows_build_container.sh
 #build latest Docker runner image:
 /bin/bash -x create_docker_to_run_megadepth.sh
