16164412d02e657cab423a4566b21eb39f53a343
ChristopherWilks <broadsword@gmail.com>
2022-09-24 12:00:02 -0400

    use proper name for hbb container

--- run_hbb_build_container.sh
+++ run_hbb_build_container.sh
@@ -1,3 +1,5 @@
 #for interactive debugging switch to just bash shell
-#docker run --ulimit core=-1 -it --rm --volume `pwd`:/build:rw --entrypoint=/bin/bash de2c52df2597 -i
-docker run --ulimit core=-1 -it --rm --volume `pwd`:/build:rw --entrypoint=/build/build_no_container_hbb.sh de2c52df2597 -i
+#older image ids: de2c52df2597, 555a36ef79dd
+docker run --ulimit core=-1 -it --rm --volume `pwd`:/build:rw --entrypoint=/build/build_no_container_hbb.sh docker.pkg.github.com/phusion/holy-build-box/hbb-64cmake:latest -i
+mv -f megadepth megadepth_hbb
+ln -fs megadepth_hbb megadepth
