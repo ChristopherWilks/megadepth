6e2e43c05d221d3d3a095c6d6eb70b35e45785ac
ChristopherWilks <broadsword@gmail.com>
2022-09-24 11:57:08 -0400

    no longer used _statlib as suffix

--- create_docker_to_run_megadepth.sh
+++ create_docker_to_run_megadepth.sh
@@ -1,5 +1,6 @@
+#to be run after ./run_hbb_build_container.sh (produces actual megadepth binary referenced here)
 mkdir -p docker.run.build
-rsync -av megadepth_statlib docker.run.build/
+rsync -av megadepth docker.run.build/
 rsync -av Dockerfile.run docker.run.build/Dockerfile
 pushd docker.run.build/
 VER=$(cat ../VERSION)
